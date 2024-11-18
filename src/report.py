0f15407c6d753752b9c883c51e929634f7811530
juckerj <39263956+jonasjucker@users.noreply.github.com>
2024-11-18 12:29:18 +0100

    Transition to multibranch  (#30)
    * add
    
    * move
    
    * post to issue directly
    
    * adjust
    
    * remove keywords file
    
    * cleanup unused actions
    
    * cleanup

--- src/report.py
+++ src/report.py
@@ -1,6 +1,5 @@
 import requests
 import argparse
-import json
 
 class GitHubRepo:
 
@@ -8,36 +7,35 @@ class GitHubRepo:
         self.group: str = group
         self.repo: str = repo
         self.auth_token: str = auth_token
+        self.headers = {'Content-Type': 'application/json'}
+        self.headers['Authorization'] = 'token ' + self.auth_token
 
     def comment(self, issue_id: str, text: str) -> None:
         url = f'https://api.github.com/repos/{self.group}/{self.repo}/issues/{issue_id}/comments'
 
-        headers = {'Content-Type': 'application/json'}
-        if self.auth_token is not None:
-            headers['Authorization'] = 'token ' + self.auth_token
-
-        requests.post(url, headers=headers, json={'body': text})
+        requests.post(url, headers=self.headers, json={'body': text})
 
     def update_labels(self, issue_id, remove_label, add_label):
-        headers = {'Content-Type': 'application/json'}
-
-        if self.auth_token is not None:
-            headers['Authorization'] = 'token ' + self.auth_token
 
         if remove_label:
             url = f'https://api.github.com/repos/{self.group}/{self.repo}/issues/{issue_id}/labels/{remove_label}'
-            requests.delete(url, headers=headers)
+            requests.delete(url, headers=self.headers)
         if add_label:
             url = f'https://api.github.com/repos/{self.group}/{self.repo}/issues/{issue_id}/labels'
-            requests.post(url, headers=headers, json={'labels': [add_label]})
+            requests.post(url, headers=self.headers, json={'labels': [add_label]})
+    
+    def get_issue(self, issue_id: str) -> dict:
+        url = f'https://api.github.com/repos/{self.group}/{self.repo}/issues/{issue_id}'
+        response = requests.get(url, headers=self.headers)
+        return response.json()['body']
+
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--auth_token', type=str, required=False)
-    parser.add_argument('--issue_id', type=str, required=True)
+    parser.add_argument('--issue_id_file', type=str, required=True)
     parser.add_argument('--hash-file', type=str, required=True)
-    parser.add_argument('--keyword-file', type=str, required=True)
 
     # Create a mutually exclusive group for the --abort and --failure arguments
     group = parser.add_mutually_exclusive_group()
@@ -53,28 +51,23 @@ if __name__ == "__main__":
     with open(args.hash_file, 'r') as f:
         hash = f.read()
 
-    with open(args.keyword_file, 'r') as f:
-        keywords = json.load(f)
-        PR_DELETE = keywords['PR_DELETE']
-        PR_FAIL = keywords['PR_FAIL']
-        PR_ABORT = keywords['PR_ABORT']
-        PR_SUCCESS = keywords['PR_SUCCESS']
-
+    with open(args.issue_id_file, 'r') as f:
+        issue_id = f.read()
 
     url = f'https://data.iac.ethz.ch/extpar-request/{hash}'
 
-    repo.comment(issue_id=args.issue_id, text=f'{url}')
+    fail =f"Something went wrong. Please check the [logfiles]({url}) for more information."
+    abort = f"Your request has been aborted. Please check the [logfiles]({url}) for more information."
+    success = f"Your data is ready for up to 7 days under this [link]({url})."
 
     if args.failure:
-        repo.comment(issue_id=args.issue_id, text=PR_FAIL)
+        repo.comment(issue_id=issue_id, text=fail)
         add_label = 'failed'
     elif args.abort:
-        repo.comment(issue_id=args.issue_id, text=PR_ABORT)
+        repo.comment(issue_id=issue_id, text=abort)
         add_label = 'aborted'
     else:
-        repo.comment(issue_id=args.issue_id, text=PR_SUCCESS)
+        repo.comment(issue_id=issue_id, text=success)
         add_label = 'completed'
 
-    repo.update_labels(issue_id=args.issue_id, remove_label='submitted', add_label=add_label)
-    repo.comment(issue_id=args.issue_id, text=PR_DELETE)
-
+    repo.update_labels(issue_id=issue_id, remove_label='submitted', add_label=add_label)
