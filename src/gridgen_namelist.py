20e0a3e815889b17cd1404360b425f5d672ca3a5
Jonas Jucker <jonas.jucker@c2sm.ethz.ch>
2024-11-19 17:03:54 +0100

    prepare workdir

--- src/gridgen_namelist.py
+++ src/gridgen_namelist.py
@@ -1,12 +1,14 @@
 import json
 import argparse
+import os
+import shutil
 
 def load_config(config_file):
     with open(config_file, 'r') as f:
         config = json.load(f)
     return config
 
-def write_global_namelist(config):
+def write_global_namelist(config,wrk_dir):
     # Set default values
     parent_id = 0
     lwrite_parent = True
@@ -29,25 +31,37 @@ def write_global_namelist(config):
 """
 
     # Write the namelist content to a file
-    with open('icontools/nml_gridgen', 'w') as f:
+    with open(os.path.join(wrk_dir,'nml_gridgen'), 'w') as f:
         f.write(namelist_content)
 
+    # write filename to grid.txt for extpar
+    with open(os.path.join(wrk_dir,'grid.txt'), 'w') as f:
+        f.write(f'{dom_outfile}_DOM01.nc')
+
+def main(workspace, config_path):
+    # Create directories
+    extpar_dir = os.path.join(workspace, 'extpar')
+    icontools_dir = os.path.join(workspace, 'icontools')
+    os.makedirs(extpar_dir, exist_ok=True)
+    os.makedirs(icontools_dir, exist_ok=True)
+
+    # Copy config.json to extpar directory
+    shutil.copy(config_path, os.path.join(extpar_dir, 'config.json'))
+
+    # Load config and write namelist
+    config = load_config(config_path)
+    config = config['icontools']
+
+    if config["region_type"] == 1:
+        write_global_namelist(config, icontools_dir)
+    else:
+        print("Region type not supported")
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Extract settings from config file")
+    parser = argparse.ArgumentParser(description="Setup workspace and generate namelist")
+    parser.add_argument('--workspace', type=str, required=True, help="Path to the workspace directory")
     parser.add_argument('--config', type=str, required=True, help="Path to the configuration file")
 
     args = parser.parse_args()
 
-    config = load_config(args.config)
-    config = config['icontools']
-
-    if config["region_type"] == 1:
-        write_global_namelist(config)
-    else:
-        print("Region type not supported")
-
-    grid_level = config.get('grid_level')
-    grid_root = config.get('grid_root')
-    region_type = config.get('region_type')
-    outfile = config.get('outfile')
+    main(args.workspace, args.config)
